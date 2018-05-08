40cb63f86b17a25fef9a0a8dbbf81ae730fd8ab6
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2018-05-08 17:41:30 +0200

    Added main.go skeleton

---
+++ main.go
@@ -0,0 +1,78 @@
+// Package godiffsub contains the main function for running the executable.
+//
+// Installation
+//
+//     go get -u github.com/kamphaus/godiffsub
+//
+// Usage
+//
+//     godiffsub -src filea.go -from fileb.go
+//
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/kamphaus/godiffsub/program"
+)
+
+var stderr io.Writer = os.Stderr
+
+type inputDataFlags []string
+
+func (i *inputDataFlags) String() (s string) {
+	for pos, item := range *i {
+		s += fmt.Sprintf("Flag %d. %s\n", pos, item)
+	}
+	return
+}
+
+func (i *inputDataFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+var (
+	versionFlag = flag.Bool("v", false, "print the version and exit")
+	helpFlag    = flag.Bool("h", false, "print help information")
+	srcFlags    inputDataFlags
+	fromFlags   inputDataFlags
+)
+
+func init() {
+	flag.Var(&srcFlags, "src", "Files whose functions, variables and constants should be considered.")
+	flag.Var(&fromFlags, "from", "Files from which any functions, variables and constants having the same name as the considered ones should be removed.")
+}
+
+func main() {
+	code := runCommand()
+	if code != 0 {
+		os.Exit(code)
+	}
+}
+
+func runCommand() int {
+
+	flag.Usage = func() {
+		usage := "Usage: %s [<flags>]\n\n"
+		usage += "Flags:\n"
+		fmt.Fprintf(stderr, usage, os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println(program.Version)
+		return 0
+	}
+
+	if flag.NArg() < 1 || *helpFlag {
+		flag.Usage()
+		return 1
+	}
+
+	return 0
+}
---
+++ program/version.go
@@ -0,0 +1,9 @@
+package program
+
+// Version can be requested through the command line with:
+//
+//     godiffsub -v
+//
+// See https://github.com/kamphaus/godiffsub/wiki/Release-Process
+const Version = "v0.0.1 2018-05-08"
+
